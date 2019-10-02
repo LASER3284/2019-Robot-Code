b9057f4e9e49a7a92f4ff45f80f5d96a38312be5
jaredp1 <33207082+jaredp1@users.noreply.github.com>
2019-10-02 12:07:51 -0500

    Rewrite DriveTick, Remove Camera
    Rewrote DriveTick to support actual ArcadeDrive in low gear, and CurvatureDrive in high gear.
    Fixed clamping output values, used a proper clamp instead of a divisor.
    Removed extraneous variables relating to Drive.
    
    Removed Camera from Vision class, which will decrease latency, increase performance, remove overhead from the RoboRio, and fix the Watchdog issues we've been having.
    Removed unused methods in Vision class.
    Removed extraneous variables no longer used in Vision class.

--- src/main/include/Vision.h
+++ src/main/include/Vision.h
@@ -13,17 +13,11 @@
 #include <frc/WPILib.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <cameraserver/CameraServer.h>
-///#include "VL53L1X.h"
 
 class CDrive;
 
 using namespace cs;
 using namespace cv;
-
-// Camera constants.
-const int nImageWidth			=  320;
-const int nImageHeight			=  240;
-const int nCameraFPS				=  	15;
 /////////////////////////////////////////////////////////////////////////////
 
 class CVision
@@ -32,20 +26,11 @@ public:
 	CVision(CDrive* pDrive);
 	~CVision();
 	void Init();
-	void UpdateDistance();
-	void CameraTick();
 	void DriveTick();
-	void SwitchCamera(int nCameraID);
 
 private:
 	// Object pointers.
-	cs::UsbCamera			m_csFrontCamera;		
-	cs::CvSink				m_csCameraSink;
-	cs::CvSource			m_csCameraSource;
-	cv::Mat					m_Image;
-
 	CDrive*			m_pDrive;
-///	VL53L1X*		m_pDistanceSensor;
 };
 /////////////////////////////////////////////////////////////////////////////
 #endif
